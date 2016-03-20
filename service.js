545a453bf07b38f234a8763c67e7f1cd3f640106
Bitterulf <s_schlender@hotmail.com>
2016-03-20 17:23:18 +0100

    Add more abstraction so we get a possible error as first parameter.

--- service.js
+++ service.js
@@ -1,9 +1,23 @@
 var axon = require('axon');
-var interfaceSocket = axon.socket('req');
 
-interfaceSocket.connect(3000, 'localhost');
+var InterfaceManager = function(host, port) {
+  this.socket = axon.socket('req');
+  this.socket.connect(3000, 'localhost');
+};
 
-interfaceSocket.send('logger', function(interfaceData){
+InterfaceManager.prototype.send = function(data, cb) {
+  this.socket.send(data, function(result) {
+    cb(null, result);
+  });
+};
+
+var im = new InterfaceManager(3000, 'localhost');
+
+// var interfaceSocket = axon.socket('req');
+
+// interfaceSocket.connect(3000, 'localhost');
+
+im.send('logger', function(err, interfaceData){
   var replySocket = axon.socket('rep');
   replySocket.connect(interfaceData.producerPort, 'localhost');
 
