4da8eed4970c08519dd0bb754fa0b38260e5d895
Bitterulf <s_schlender@hotmail.com>
2016-03-20 18:08:36 +0100

    Adding bluebird to create promises for a better overview.

--- service.js
+++ service.js
@@ -1,34 +1,57 @@
 var axon = require('axon');
 
+var Promise = require('bluebird');
+
 var InterfaceManager = function(host, port) {
   this.socket = axon.socket('req');
   this.socket.connect(3000, 'localhost');
 };
 
 InterfaceManager.prototype.send = function(data, cb) {
-  this.socket.send(data, function(result) {
-    cb(null, result);
+  this.socket.send(data, function(interfaceData) {
+    cb(null, {interfaceData: interfaceData});
   });
 };
 
-var im = new InterfaceManager(3000, 'localhost');
+var im = Promise.promisifyAll(new InterfaceManager(3000, 'localhost'));
 
-// var interfaceSocket = axon.socket('req');
+var addReplySocket = Promise.promisify(function(result, cb){
+  if (!result.sockets) result.sockets = {};
+  result.sockets.reply = axon.socket('rep');
+  result.sockets.reply.connect(result.interfaceData.producerPort, 'localhost');
 
-// interfaceSocket.connect(3000, 'localhost');
+  cb(null, result);
+});
 
-im.send('logger', function(err, interfaceData){
-  var replySocket = axon.socket('rep');
-  replySocket.connect(interfaceData.producerPort, 'localhost');
+var addRequestSocket = Promise.promisify(function(result, cb){
+  if (!result.sockets) result.sockets = {};
+  result.sockets.request = axon.socket('req');
+  result.sockets.request.connect(result.interfaceData.consumerPort, 'localhost');
 
-  replySocket.on('message', function(data, reply){
+  cb(null, result);
+});
+
+var addReplyLogic = Promise.promisify(function(result, cb){
+  result.sockets.reply.on('message', function(data, reply){
     reply(data+'bar');
   });
 
-  var requestSocket = axon.socket('req');
-  requestSocket.connect(interfaceData.consumerPort, 'localhost');
+  cb(null, result);
+});
 
-  requestSocket.send('foo', function(result){
+var testInterfaces = Promise.promisify(function(result, cb){
+  result.sockets.request.send('foo', function(result){
     console.log(result);
   });
+
+  cb(null, result);
 });
+
+im.sendAsync('logger')
+  .then(addReplySocket)
+  .then(addRequestSocket)
+  .then(addReplyLogic)
+  .then(testInterfaces)
+  .then(function(result){
+    console.log('ready to rock!', result);
+  });
