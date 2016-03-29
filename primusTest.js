788782661c46c887585005af14b80d6fa07a6a1c
Bitterulf <s_schlender@hotmail.com>
2016-03-29 14:56:49 +0200

    Add Primus with hapi.

---
+++ primusTest.js
@@ -0,0 +1,44 @@
+var Primus = require('primus');
+
+var Path = require('path');
+var Hapi = require('hapi');
+var Inert = require('inert');
+
+var server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
+
+server.connection({ port: 8080 });
+
+server.register(Inert, function(){});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true
+        }
+    }
+});
+
+server.start(function(err) {
+  if (err) {
+      throw err;
+  }
+
+  console.log('Server running at:', server.info.uri);
+
+  var primus = new Primus(server.listener, {});
+  primus.on('connection', function (spark) {
+    spark.write({ action: 'init' });
+  });
+});
